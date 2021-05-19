414cbeb25182a8040e372eac5ae67bd48dfa7482
SherryS997 <kratosreborn32@protonmail.com>
2021-05-19 16:45:34 +0530

    dd

--- Scientific Computing with Python/Class Projects/web_scrapper(BeautifulSoup).py
+++ Scientific Computing with Python/Class Projects/web_scrapper(BeautifulSoup).py
@@ -1,8 +1,13 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter url = ')
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context= ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 #Retrive all anchor tags
