8eae98610a45610b5281fc74cfcb7703867370b3
SherryS997 <kratosreborn32@protonmail.com>
2021-05-27 14:38:48 +0530

    dd

--- Scientific Computing with Python/DIY_Projects/1. Arithmatic Formatter/arithmetic_arranger.py
+++ Scientific Computing with Python/DIY_Projects/1. Arithmatic Formatter/arithmetic_arranger.py
@@ -1,3 +1,16 @@
+def calculator(list):
+    ans = []
+    for dd in list:
+        nn = 0
+        cc = dd.rsplit()
+        if cc[1] == '+':
+            nn = int(cc[0]) + int(cc[2])
+        else:
+            nn = int(cc[0]) - int(cc[2])
+        ans.append(nn)
+    return ans
+
+
 def arithmetic_arranger(problems, binary = None):
     arranged_problems = 'Cum'
 
