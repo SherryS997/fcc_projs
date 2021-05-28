af7b36b7c4a2052946509f252acbaabba6755595
SherryS997 <kratosreborn32@protonmail.com>
2021-05-28 18:16:08 +0530

    dd

---
+++ Scientific Computing with Python/DIY_Projects/2. Time Calculator/test.py
@@ -0,0 +1,5 @@
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+periods = ['AM', 'PM']
+minutes = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09']
+for x in range(10, 60): minutes.append(str(x))
+
