4650e113ee4f189181210bdc6a1ae246873de54e
SherryS997 <kratosreborn32@protonmail.com>
2021-05-28 14:45:26 +0530

    dd

--- Scientific Computing with Python/DIY_Projects/1. Arithmatic Formatter/arithmetic_arranger.py
+++ Scientific Computing with Python/DIY_Projects/1. Arithmatic Formatter/arithmetic_arranger.py
@@ -12,25 +12,16 @@ def is_error(list):
 
 def error(list):
     ans = None
-    if len(list) > 5:
-        ans = 'Error: Too many problems.'
-        return ans
-    for dd in list:
-        cc = dd.rsplit()
-        if len(cc) == 3:
-            if cc[1] != '+' and cc[1] != '-':
-                ans = "Error: Operator must be '+' or '-'."
-                return ans
-            elif not cc[0].isdecimal() or not cc[2].isdecimal():
-                ans = 'Error: Numbers must only contain digits.'
-                return ans
-            elif len(cc[0]) > 4 or len(cc[2]) > 4:
-                ans = 'Error: Numbers cannot be more than four digits.'
-                return ans
-        else:
-            ans = 'Other issues'
-            return ans
-    return None
+    if len(list) > 5: ans = 'Error: Too many problems.'
+    else:
+        for dd in list:
+            cc = dd.rsplit()
+            if len(cc) == 3:
+                if cc[1] != '+' and cc[1] != '-': ans = "Error: Operator must be '+' or '-'."
+                elif not cc[0].isdecimal() or not cc[2].isdecimal(): ans = 'Error: Numbers must only contain digits.'
+                elif len(cc[0]) > 4 or len(cc[2]) > 4: ans = 'Error: Numbers cannot be more than four digits.'
+            else: ans = 'Other issues'
+    return ans
 
 
 def calculator(list):
@@ -43,7 +34,42 @@ def calculator(list):
     return ans
 
 
-def arithmetic_arranger(problems, tosolve = False):
-    arranged_problems = 'gg'
-
+def arithmetic_arranger(problems, to_solve = False):
+    if is_error(problems): return error(problems)
+    arranged_problems = ''
+    req = []
+    if to_solve: ans = calculator(problems)
+    for problem in problems:
+        dd = problem.rsplit()
+        if len(dd[0]) > len(dd[2]):
+            spaces = (len(dd[0]) + 1) - len(dd[2])
+            dd[0] = (' ' * 2) + dd[0]
+            dd[1] = dd[1] + (' ' * spaces) + dd[2]
+            dd[2] = '-' * len(dd[0])
+            if to_solve == True:
+                this_ans = str(ans[problems.index(problem)])
+                this_ans = (' ' * (len(dd[0]) - len(this_ans))) + this_ans
+                dd.append(this_ans)
+        else:
+            spaces = (len(dd[2]) + 2) - len(dd[0])
+            dd[0] = (' ' * spaces) + dd[0]
+            dd[1] = dd[1] + ' ' + dd[2]
+            dd[2] = '-' * len(dd[0])
+            if to_solve == True:
+                this_ans = str(ans[problems.index(problem)])
+                this_ans = (' ' * (len(dd[0]) - len(this_ans))) + this_ans
+                dd.append(this_ans)
+        for char in dd:
+            req.append(char)
+    if to_solve: aa = len(problems)
+    else: aa = 3
+    for x in range(aa):
+        y = x
+        while y < len(req):
+            word = req[y]
+            arranged_problems += word
+            arranged_problems += ' ' * 4
+            if to_solve: y += 4
+            else: y += 3
+        if x < (aa - 1): arranged_problems += '\n'
     return arranged_problems
